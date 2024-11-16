761df066e177860902d70193534f222b0983ec14
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-11-16 14:12:52 +0200

    add input number by space

--- Program.cs
+++ Program.cs
@@ -6,6 +6,16 @@
         {
             var random = new Random();
 
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"Exeption: {ex}");
+            }
+
             Console.WriteLine("Enter the lower bound of the random number: ");
             var lowerMaxRandomBound = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter the upper bound of the random number: ");
@@ -17,6 +27,9 @@
             }
             Console.WriteLine("Enter the size of the array: ");
             int size = Convert.ToInt32(Console.ReadLine());
+            if (size < 0) { 
+                throw new Exception("The size of the array cannot be negative");
+            }
             var arr = new int[size];
             for (int i = 0; i < size; i++)
             {
@@ -33,11 +46,21 @@
 
             Console.WriteLine();
             var arr_three = new int[3];
-            for (int i = 0; i < 3; i++)
-            {
-                Console.WriteLine($"Enter number {i}: ");
-                arr_three[i] = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter numbers (by space): ");
+            string? arr_three_str = Console.ReadLine();
+
+            if (arr_three_str == null) {
+                throw new Exception("The input cannot be null");
             }
+
+            arr_three = Array.ConvertAll(arr_three_str.Split(' '), int.Parse);//to convert the string to int array
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    Console.WriteLine($"Enter number {i}: ");
+            //    arr_three[i] = Convert.ToInt32(Console.ReadLine());
+            //}
 
             int three_in_arr = 0;
             for (int i = 0; i < arr.Length - 2; i++)
