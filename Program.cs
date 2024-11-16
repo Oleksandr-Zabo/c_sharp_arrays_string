78a10b7ee0d1dfe2fceef293e4ccc14182dacadd
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-11-16 14:30:15 +0200

    ex. 6 lab had done

--- Program.cs
+++ Program.cs
@@ -1,10 +1,34 @@
 ﻿namespace c_sharp_arrays_string
-{//dev
+{//lab-ex.6
     public class Program
     {
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                Console.WriteLine("Enter sentence: ");
+                string? input = Console.ReadLine();
+
+                if (input == null) {
+                    throw new ArgumentNullException("Input is null");
+                }
+                string[] words = input.Split(new char[] { ' ', '\t', '\n', '\r', ',', ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (string word in words)
+                {
+                    Console.WriteLine(word);
+                }
+                int wordCount = words.Length;
+
+
+                Console.WriteLine($"Number of word in sentence: {wordCount}");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"Exeption {ex}");
+            }
+            
         }
     }
 }
